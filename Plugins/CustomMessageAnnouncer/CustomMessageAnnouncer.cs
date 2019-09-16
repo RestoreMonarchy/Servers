cd854479e6fef2b96a799fad2de47abb6abb5f80
RestoreMonarchy <35507241+RestoreMonarchy@users.noreply.github.com>
2019-09-16 19:44:30 +0200

    Finished CustomMessageAnnouncer plugin

---
+++ Plugins/CustomMessageAnnouncer/CustomMessageAnnouncer.cs
@@ -0,0 +1,36 @@
+﻿using Rocket.Core.Commands;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using SDG.Unturned;
+using System.Timers;
+using UnityEngine;
+
+namespace CustomMessageAnnouncer
+{
+    public class CustomMessageAnnouncer : RocketPlugin<CustomMessageAnnouncerConfiguration>
+    {
+        private Timer timer;
+        private int index = 0;
+
+        protected override void Load()
+        {
+            timer = new Timer(Configuration.Instance.MessageInterval * 1000);
+            timer.AutoReset = true;
+            timer.Elapsed += SendMessage;
+            timer.Start();
+        }
+
+        private void SendMessage(object sender, ElapsedEventArgs e)
+        {
+            if (index >= Configuration.Instance.Messages.Count)
+            {
+                index = 0;
+            }
+            
+            ChatManager.serverSendMessage(Configuration.Instance.Messages[index].Text.Replace('{', '<').Replace('}', '>'), 
+                UnturnedChat.GetColorFromName(Configuration.Instance.Messages[index].Color, Color.green), 
+                iconURL: Configuration.Instance.Messages[index].IconUrl, useRichTextFormatting: true);
+            index++;
+        }
+    }
+}
---
+++ Plugins/CustomMessageAnnouncer/CustomMessageAnnouncerConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Rocket.API;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace CustomMessageAnnouncer
+{
+    public class CustomMessageAnnouncerConfiguration : IRocketPluginConfiguration
+    {
+        public double MessageInterval { get; set; }
+        [XmlArrayItem("Message")]
+        public List<Message> Messages { get; set; }
+        public void LoadDefaults()
+        {
+            MessageInterval = 300;
+            Messages = new List<Message>()
+            {
+                new Message("{size=22}You are playing on RestoreMonarchy!{/size}", "https://i.imgur.com/vWoACbH.png", "yellow"),
+                new Message("{size=22}Visit RestoreMonarchy.com{/size}", "https://i.imgur.com/vWoACbH.png", "yellow")
+            };
+        }
+    }
+
+    public sealed class Message
+    {
+        public Message(string text, string iconUrl, string color)
+        {
+            Text = text;
+            IconUrl = iconUrl;
+            Color = color;
+        }
+
+        public Message() { }
+
+        [XmlAttribute]
+        public string Text { get; set; }
+        [XmlAttribute]
+        public string IconUrl { get; set; }
+        [XmlAttribute]
+        public string Color { get; set; }
+    }
+}
