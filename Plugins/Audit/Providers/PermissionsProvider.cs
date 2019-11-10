6eb4447276af1d0fcc1dfc8deba62cd017f53d7d
RestoreMonarchy <35507241+RestoreMonarchy@users.noreply.github.com>
2019-11-11 00:42:06 +0100

    created audit plugin

---
+++ Plugins/Audit/Providers/PermissionsProvider.cs
@@ -0,0 +1,122 @@
+﻿using Core.Models;
+using RestoreMonarchy.Audit.Utilities;
+using Rocket.API;
+using Rocket.API.Serialisation;
+using Rocket.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using Timer = System.Timers.Timer;
+
+namespace RestoreMonarchy.Audit.Providers
+{
+    public class PermissionsProvider : IRocketPermissionsProvider
+    {
+        private readonly AuditPlugin pluginInstance;
+        private readonly Timer timer;
+        public List<Rank> PlayerRanks { get; set; }
+        private List<RocketPermissionsGroup> permissionGroups { get; set; }
+
+        public PermissionsProvider(AuditPlugin plugin)
+        {
+            pluginInstance = plugin;
+            timer = new Timer(plugin.Configuration.Instance.RefreshInterval);
+            timer.Elapsed += (o, e) => RefreshPermissions();            
+            RefreshPermissions();
+            timer.Start();
+        }
+
+        public void RefreshPermissions()
+        {            
+            ThreadPool.QueueUserWorkItem((i) => 
+            {
+                Logger.LogWarning("Refreshing permissions...");
+                using (WebClient wc = pluginInstance.Client)
+                {
+                    PlayerRanks = wc.DownloadJson<List<Rank>>(pluginInstance.Configuration.Instance.APIUrl + "/ranks/server");
+                }
+
+                Logger.LogWarning($"{PlayerRanks.Count} player ranks have been loaded!");
+                List<RocketPermissionsGroup> groups = new List<RocketPermissionsGroup>();
+                foreach (var rank in PlayerRanks)
+                {
+                    var perms = rank.PermissionTags.Split(',').Select(p => new Permission(p)).ToList();
+                    groups.Add(new RocketPermissionsGroup(rank.RankId.ToString(), rank.Name, string.Empty, rank.Members, perms));
+                }
+                permissionGroups = groups;
+
+                Logger.LogWarning($"{groups.Count} groups have been loaded!");
+
+            });
+        }
+        
+
+        public RocketPermissionsProviderResult AddGroup(RocketPermissionsGroup group)
+        {
+            Logger.LogWarning("AddGroup is not supported by PermissionsProvider");
+            return RocketPermissionsProviderResult.UnspecifiedError;
+        }
+
+        public RocketPermissionsProviderResult AddPlayerToGroup(string groupId, IRocketPlayer player)
+        {
+            Logger.LogWarning("AddPlayerToGroup is not supported by PermissionsProvider");
+            return RocketPermissionsProviderResult.UnspecifiedError;
+        }
+
+        public RocketPermissionsProviderResult DeleteGroup(string groupId)
+        {
+            Logger.LogWarning("DeleteGroup is not supported by PermissionsProvider");
+            return RocketPermissionsProviderResult.UnspecifiedError;
+        }
+
+        public RocketPermissionsGroup GetGroup(string groupId)
+        {
+            return permissionGroups.FirstOrDefault(x => x.DisplayName.Equals(groupId, StringComparison.OrdinalIgnoreCase) || x.Id.ToString() == groupId);            
+        }
+
+        public List<RocketPermissionsGroup> GetGroups(IRocketPlayer player, bool includeParentGroups = false)
+        {
+            return permissionGroups.Where(x => x.Members.Exists(y => y == player.Id)).ToList();
+        }
+
+        public List<Permission> GetPermissions(IRocketPlayer player)
+        {
+            var groups = GetGroups(player);
+            List<Permission> permissions = new List<Permission>();
+            foreach (var group in groups)
+            {
+                group.Permissions.ForEach((item) => permissions.Add(item));
+            }
+            return permissions;
+        }
+
+        public List<Permission> GetPermissions(IRocketPlayer player, List<string> requestedPermissions)
+        {
+            return GetPermissions(player).Where(x => requestedPermissions.Exists(y => y.Equals(x.Name, StringComparison.OrdinalIgnoreCase))).GroupBy(x=> x.Name).Select(x => x.First()).ToList();
+        }
+
+        public bool HasPermission(IRocketPlayer player, List<string> requestedPermissions)
+        {
+            return player.IsAdmin ? true : GetPermissions(player, requestedPermissions).Count == requestedPermissions.Count ? true : false;
+        }
+
+        public void Reload()
+        {
+            RefreshPermissions();
+        }
+
+        public RocketPermissionsProviderResult RemovePlayerFromGroup(string groupId, IRocketPlayer player)
+        {
+            Logger.LogWarning("RemovePlayerFromGroup is not supported by PermissionsProvider");
+            return RocketPermissionsProviderResult.UnspecifiedError;
+        }
+
+        public RocketPermissionsProviderResult SaveGroup(RocketPermissionsGroup group)
+        {
+            Logger.LogWarning("SaveGroup is not supported by PermissionsProvider");
+            return RocketPermissionsProviderResult.UnspecifiedError;
+        }
+    }
+}
---
+++ Plugins/UsefulCommands/UsefulCommandsPlugin.cs
@@ -0,0 +1,76 @@
+﻿using Rocket.API;
+using Rocket.API.Collections;
+using Rocket.Core.Commands;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System.Linq;
+using System.Timers;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace UsefulCommands
+{
+    public class UsefulCommandsPlugin : RocketPlugin
+    {
+        protected override void Load()
+        {
+            Logger.Log($"{Name} {Assembly.GetName().Version} has been loaded!");
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log($"{Name} has been unloaded!");
+        }
+
+        [RocketCommand("maxskills", "Gives you maxskills", AllowedCaller: AllowedCaller.Player)]
+        public void MaxskillsCommand(IRocketPlayer caller, string[] args)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            player.MaxSkills();
+            UnturnedChat.Say(caller, Translate("MaxskillsSuccess"));
+        }
+
+        [RocketCommand("restart", "Restarts the server within the time and optionally tells the reason", "<time> [reason]", AllowedCaller.Both)]
+        public void RestartCommand(IRocketPlayer caller, string[] args)
+        {
+            Color color = Color.red;
+
+            if (args.Length == 0 || !double.TryParse(args[0], out double time))
+            {
+                UnturnedChat.Say(caller, Translate("RestartFormat"));
+                return;
+            }
+
+            string reason = string.Join(" ", args.Skip(1));
+
+            UnturnedChat.Say(Translate("RestartNotify", time), color);
+            Timer timer = new Timer(1000);
+            timer.Elapsed += (o, e) =>
+            {
+                if (time != 0 && (time % 10 == 0 || (time <= 30 && time % 5 == 0)))
+                {
+                    UnturnedChat.Say(Translate("RestartNotify", time), color);
+                }
+                
+                if (--time == 0)
+                {
+                    UnturnedChat.Say(Translate("RestartSuccess"), color);
+                    SaveManager.save();
+                    Provider.shutdown(0, reason);
+                    timer.Enabled = false;
+                }
+            };
+            timer.Start();
+        }
+
+        public override TranslationList DefaultTranslations => new TranslationList()
+        {
+            { "MaxskillsSuccess", "Successfully given you maxskills!" },
+            { "RestartFormat", "Format: /restart <time> [reason]" },
+            { "RestartNotify", "Server restart in {0} seconds!" },
+            { "RestartSuccess", "Server is restarting..." }
+        };
+    }
+}
